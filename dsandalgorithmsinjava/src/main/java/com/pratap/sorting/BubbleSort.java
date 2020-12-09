f203d08dc308fcc46f91aa0d2efb342987fc95d4
pratap_narayan <pratap_narayan@apple.com>
2020-12-09 21:16:54 +0530

    bubble sort feature

---
+++ dsandalgorithmsinjava/src/main/java/com/pratap/sorting/BubbleSort.java
@@ -0,0 +1,26 @@
+package com.pratap.sorting;
+
+import com.pratap.utils.DSUtils;
+
+public class BubbleSort {
+
+	public static void main(String[] args) {
+
+		int[] intArr = {20, 35, -15, 7, 55, 1, -22};
+		
+		
+		for(int lastUnsortedIndex = intArr.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
+		
+			for(int i = 0; i < lastUnsortedIndex; i++) {
+				// swap consecutive nums, if first num is greater than immediate num
+				if(intArr[i] > intArr[i+1]) {
+					DSUtils.swapByTempVar(intArr, i, i+1);
+				}
+			}
+		}
+		for(int i = 0; i < intArr.length; i++) {
+			System.out.println(intArr[i]);
+		}
+	}
+
+}
---
+++ dsandalgorithmsinjava/src/main/java/com/pratap/utils/DSUtils.java
@@ -0,0 +1,25 @@
+package com.pratap.utils;
+
+public final class DSUtils {
+
+	// swapping two int, in intArray
+	// 1. temp Swapping
+	public static void swapByTempVar(int[] arr, int i, int j) {
+		int temp = arr[j];
+		arr[j] = arr[i];
+		arr[i] = temp;
+	}
+	
+	// 2. without temp Swapping
+	public static void swapWithoutTempVar(int[] arr, int i, int j) {
+		arr[i] = arr[i] * arr[j];
+		arr[j] = arr[i] / arr[j];
+		arr[i] = arr[i] / arr[j];
+	}
+	// 3. XOR swapping - more fast
+	public static void swapByXOR(int[] arr, int i, int j) {
+		arr[i] = arr[i] ^ arr[j];
+		arr[j] = arr[i] ^ arr[j];
+		arr[i] = arr[i] ^ arr[j];
+	}
+}
